c61af5598b83d71d543de5a290e719dc302df9be
uday634 <uk8096386837@gmail.com>
2023-08-09 13:16:38 +0530

    threed commit

--- DOM.js
+++ DOM.js
@@ -11,17 +11,20 @@
 // document.all[10].textContent='Hello'
 // console.log(document.forms[0])
 // console.log(document.links)
-console.log(document.getElementById('header-title'))
-var headerTitle=document.getElementById('header-title')
-console.log(headerTitle)
-headerTitle.textContent = 'Hello';
-headerTitle.innerText= 'Goodbye';
-var items= document.getElementsByClassName('list-group-item');
-console.log(items)
-console.log(items[2]);
-items[2].textContent = 'hello'
-items[1].style.fontWeight = 'bold'
-items[1].style.backgroundColor = 'yellow';
-for(var i = 0 ; i < items.length ; i++){
-    items[i].style.backgroundColor = "#f4f4f4"
-}
+// console.log(document.getElementById('header-title'))
+// var headerTitle=document.getElementById('header-title')
+// console.log(headerTitle)
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText= 'Goodbye';
+// var items= document.getElementsByClassName('list-group-item');
+// console.log(items)
+// console.log(items[2]);
+// items[2].textContent = 'hello'
+// items[1].style.fontWeight = 'bold'
+// items[1].style.backgroundColor = 'yellow';
+// for(var i = 0 ; i < items.length ; i++){
+//     items[i].style.backgroundColor = "#f4f4f4"
+// }
+
+// console.log(document.getElementsByTagName('li'))
+ console.log(document.getElementsByClassName('list-group-item'))
