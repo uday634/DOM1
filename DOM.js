cbae1e696a851726b5cbc74a585a4d40a54604c4
uday634 <uk8096386837@gmail.com>
2023-08-09 13:05:13 +0530

    Second change

--- DOM.js
+++ DOM.js
@@ -16,3 +16,12 @@ var headerTitle=document.getElementById('header-title')
 console.log(headerTitle)
 headerTitle.textContent = 'Hello';
 headerTitle.innerText= 'Goodbye';
+var items= document.getElementsByClassName('list-group-item');
+console.log(items)
+console.log(items[2]);
+items[2].textContent = 'hello'
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow';
+for(var i = 0 ; i < items.length ; i++){
+    items[i].style.backgroundColor = "#f4f4f4"
+}
