0cb7b3223490767168d070dde6c5d49bf3feec11
uday634 <uk8096386837@gmail.com>
2023-08-10 20:40:21 +0530

    6 commeit

--- DOM.js
+++ DOM.js
@@ -1,97 +1,45 @@
-//EXAMINE THE DOCUMENT OBJECT //
-// console.dir(document)
-// console.log(    document.domain)
-// console.log(document.URL)
-// document.title= 'uday';
-// console.log(document.title);
-// console.log(document.head)
-// console.log(document.body)
-// console.log(document.all)
-// console.log(document.all[10])
-// document.all[10].textContent='Hello'
-// console.log(document.forms[0])
-// console.log(document.links)
-// console.log(document.getElementById('header-title'))
-// var headerTitle=document.getElementById('header-title')
-// console.log(headerTitle)
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText= 'Goodbye';
-// var items= document.getElementsByClassName('list-group-item');
-// console.log(items)
-// console.log(items[2]);
-// items[2].textContent = 'hello'
-// items[1].style.fontWeight = 'bold'
-// items[1].style.backgroundColor = 'yellow';
-// for(var i = 0 ; i < items.length ; i++){
-//     items[i].style.backgroundColor = "#f4f4f4"
-// }
+var form = document.getElementById('addForm')
+var itemList = document.getElementById('items');
+var filter = document.getElementById('fliter')
 
-// console.log(document.getElementsByTagName('li'))
-// console.log(document.getElementsByClassName('list-group-item'))
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom= 'solid 4px #ccc'
-// var input = document.querySelector('input')
-// input.value = "hellow World"
+//form Submit event
+ form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var newItem = document.getElementById('item').value;
 
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value = 'Send'
+    //create a new li element
+    var li = document.createElement('li');
+    //add class name
+    li.className = 'list-group-item'
+    //add textnodewith input vaue
+    li.appendChild(document.createTextNode(newItem));
 
-// var secondchild = document.querySelector('.list-group-item:nth-child(2)');
+    //create a delelt button element
+    var deletebtn = document.createElement('button');
 
-// secondchild.style.backgroundColor = 'green'
-// var thradchild = document.querySelector(".list-group-item:nth-child(3)");
-// thradchild.style.display = "none"
+    //add class to del button
+    deletebtn.className = 'btn btn-danger btn-sm float-right delete'
+    //append text node
+    deletebtn.appendChild(document.createTextNode('X'))
+    
+    //append button to li
+    li.appendChild(deletebtn)
 
-// var itemList = document.querySelector("#items");
-// // //parentnode
-// // console.log(itemList.parentNode);
-// // itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// // console.log(itemList.parentNode.parentNode.parentNode)
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement)
+    itemList.appendChild(li);
+ });
 
-// //childNodes
-// // console.log(itemList.childNodes)
-// console.log(itemList.children)
-// console.log(itemList.children[1])
-// itemList.children[1].style.backgroundColor='yellow';
+ //delet event
+ itemList.addEventListener('click',(e) => {
+    if(e.target.classList.contains('delete')){
+        if(confirm('are  you sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
+ })
 
-// //FristChid
-// console.log(itemList.firstChild)
-
-// //firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent='Hellow world'
-
-// //LastChid
-// console.log(itemList.firstChild)
-
-// //LastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent='Hellow '
-
-// //nextSibling
-// console.log(itemList.nextSibling)
-// //nextElementSibling
-// console.log(itemList.nextElementSibling)
-
-// //prevesSibling
-// console.log(itemList.previousSibling)
-// console.log(itemList.previousElementSibling)
-// itemList.previousElementSibling.style.color = 'green'
-
-//creat a div
-var newDiv = document.createElement('div')
-newDiv.className = 'hellow'
-newDiv.id='hello1'
-newDiv.setAttribute('title','hellowDiv');
-var newDivText = document.createTextNode('hello world');
-//add text to div
-newDiv.appendChild(newDivText);
-
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-container.insertBefore(newDiv, h1)
-console.log(newDiv)
-newDiv.style.fontSize='30px'
+ filter.addEventListener('keyup',(e)=>{
+    //convert tet to lowercase 
+    var text = e.target.value.toLoweCase()
+    console.log(text);
+ })
