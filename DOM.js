e8dc17bd8125431dd58aeec0be2b793f420962a2
uday634 <uk8096386837@gmail.com>
2023-08-10 15:30:25 +0530

    commit 5

--- DOM.js
+++ DOM.js
@@ -28,16 +28,70 @@
 
 // console.log(document.getElementsByTagName('li'))
 // console.log(document.getElementsByClassName('list-group-item'))
-var header = document.querySelector('#main-header');
-header.style.borderBottom= 'solid 4px #ccc'
-var input = document.querySelector('input')
-input.value = "hellow World"
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom= 'solid 4px #ccc'
+// var input = document.querySelector('input')
+// input.value = "hellow World"
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value = 'Send'
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = 'Send'
 
-var secondchild = document.querySelector('.list-group-item:nth-child(2)');
+// var secondchild = document.querySelector('.list-group-item:nth-child(2)');
 
-secondchild.style.backgroundColor = 'green'
-var thradchild = document.querySelector(".list-group-item:nth-child(3)");
-thradchild.style.display = "none"
+// secondchild.style.backgroundColor = 'green'
+// var thradchild = document.querySelector(".list-group-item:nth-child(3)");
+// thradchild.style.display = "none"
+
+// var itemList = document.querySelector("#items");
+// // //parentnode
+// // console.log(itemList.parentNode);
+// // itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// // console.log(itemList.parentNode.parentNode.parentNode)
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement)
+
+// //childNodes
+// // console.log(itemList.childNodes)
+// console.log(itemList.children)
+// console.log(itemList.children[1])
+// itemList.children[1].style.backgroundColor='yellow';
+
+// //FristChid
+// console.log(itemList.firstChild)
+
+// //firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hellow world'
+
+// //LastChid
+// console.log(itemList.firstChild)
+
+// //LastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Hellow '
+
+// //nextSibling
+// console.log(itemList.nextSibling)
+// //nextElementSibling
+// console.log(itemList.nextElementSibling)
+
+// //prevesSibling
+// console.log(itemList.previousSibling)
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.style.color = 'green'
+
+//creat a div
+var newDiv = document.createElement('div')
+newDiv.className = 'hellow'
+newDiv.id='hello1'
+newDiv.setAttribute('title','hellowDiv');
+var newDivText = document.createTextNode('hello world');
+//add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1)
+console.log(newDiv)
+newDiv.style.fontSize='30px'
