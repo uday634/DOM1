238b0afb88594820bf3fcc6f376584e5994e0499
uday634 <uk8096386837@gmail.com>
2023-08-11 11:26:50 +0530

    7 commit

--- DOM.js
+++ DOM.js
@@ -1,6 +1,7 @@
 var form = document.getElementById('addForm')
 var itemList = document.getElementById('items');
-var filter = document.getElementById('fliter')
+var filter = document.getElementById('filter');
+
 
 //form Submit event
  form.addEventListener('submit', (e) => {
@@ -40,6 +41,23 @@ var filter = document.getElementById('fliter')
 
  filter.addEventListener('keyup',(e)=>{
     //convert tet to lowercase 
-    var text = e.target.value.toLoweCase()
+    var text = e.target.value.toLowerCase()
     console.log(text);
+ })
+
+ filter.addEventListener('keyup', (e)=>{
+   //convert text to lowercase
+   var text= e.target.value.toLowerCase();
+   //get lis
+   var items = itemList.getElementsByTagName('li')
+   //convert to an arry
+   Array.from(items).forEach((item)=>{
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) !== -1){
+         item.style.display = 'block';
+      }else{
+         item.style.display = 'none'
+      }
+   })
+
  })
